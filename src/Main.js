319843a10c539a970e2383804f21cad6ae205ae6
Nayely Rodarte <nayelyrodarte@gmail.com>
2020-02-14 20:57:05 -0600

    fixed counter function

--- src/Main.js
+++ src/Main.js
@@ -1,25 +1,21 @@
 import React from 'react';
 import styled from 'styled-components';
+import Counter from './Counter';
 
-const Header = styled.h1``;
+const Header = styled.h2`
+  font-family: 'Parisienne';
+  font-size: 2.5em;
+  font-weight: regular;
+`;
 
 class Main extends React.Component {
-  counter = () => {
-    const wedDay = new Date('2020,05,24 20:00:00');
-    let dateNow = Date.now();
-
-    let daysRemaining = Math.round((wedDay - dateNow) / (1000 * 60 * 60 * 24));
-
-    console.log(daysRemaining);
-  };
-
   render() {
     return (
       <div>
-        <Header> ¡Nos casamos! </Header>
-        <h3> Brenda & Aarón </h3>
-        <h4> 23 de mayo de 2020 </h4>
-        <h4> Faltan ${this.count} días</h4>
+        <h1> ¡Nos casamos! </h1>
+        <Header> Brenda & Aarón </Header>
+        <h1> 23 de mayo de 2020 </h1>
+        <Counter />
       </div>
     );
   }
