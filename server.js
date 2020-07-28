caba0dbb4d2978134ba05b19e074058e23ef787c
Nayely Rodarte <nayelyrodarte@gmail.com>
2020-07-27 20:11:17 -0500

    prepare for deploy

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const dotenv = require('dotenv').config();
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path');
 
 const db = process.env.MONGODB;
 const PORT = process.env.PORT || 8000;
@@ -11,6 +12,17 @@ app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
 // Define routes
 app.use('/api/guests', require('./routes/guests'));
+
+// Serve static assets in production (react)
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get(
+    '*',
+    (req,
+    (res) =>
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')))
+  );
+}
 
 mongoose
   .connect(db, {
