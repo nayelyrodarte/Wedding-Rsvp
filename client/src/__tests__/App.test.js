42466ec8bb86363250d929fa7799e473816f4bde
Nayely Rodarte <nayelyrodarte@gmail.com>
2020-07-30 16:17:37 -0500

    Delete tests files

--- client/src/__tests__/App.test.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import App from '../App';
-
-it('App renders without crashing', () => {
-  expect(shallow(<App />).length).toEqual(1);
-});
