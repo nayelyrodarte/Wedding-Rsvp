3a8fb5d5eae6cb57583137c9e33518c2ff41f87a
Nayely Rodarte <nayelyrodarte@gmail.com>
2020-09-14 11:03:48 -0500

    Add test file for App

---
+++ client/src/__tests__/App.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+
+import App from '../components/App';
+
+test('renders App component', () => {
+  render(<App />);
+});
