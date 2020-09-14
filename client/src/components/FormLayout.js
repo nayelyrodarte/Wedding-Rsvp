496dd24e745b750a1b0ad293d14981d85910425a
Nayely Rodarte <nayelyrodarte@gmail.com>
2020-09-14 16:57:17 -0500

    Change form's h2 to form-layout component

--- client/src/components/FormLayout.js
+++ client/src/components/FormLayout.js
@@ -4,6 +4,7 @@ import Message from './Message';
 const FormLayout = ({ children, error }) => {
   return (
     <section className='form-layout-component'>
+      <h2>Confirma tu asistencia</h2>
       {error && <Message msg={error} />}
       {children}
     </section>
