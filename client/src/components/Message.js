59ac19ca76995768b326dfda8851eee2f7d4b284
Nayely Rodarte <nayelyrodarte@gmail.com>
2020-09-21 13:17:57 -0500

    Fix switch error

--- client/src/components/Message.js
+++ client/src/components/Message.js
@@ -4,17 +4,14 @@ const Message = ({ msg, type }) => {
   switch (type) {
     case 'error':
       return <div className='error message'>{msg}</div>;
-      break;
     case 'charging':
       return (
         <div className='message'>
           <span className='charging'>{msg}</span>
         </div>
       );
-      break;
     default:
       return <div className='message'>{msg}</div>;
-      break;
   }
 };
 
