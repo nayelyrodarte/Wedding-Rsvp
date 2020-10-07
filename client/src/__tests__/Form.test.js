c82a3b0723aeb536513faba3aaa106e7574756be
Nayely Rodarte <nayelyrodarte@gmail.com>
2020-10-07 10:38:01 -0500

    Update form tests

--- client/src/__tests__/Form.test.js
+++ client/src/__tests__/Form.test.js
@@ -3,65 +3,28 @@ import { render, screen, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Form from '../components/Form';
 
-import { SWRConfig, cache } from 'swr';
-import { rest } from 'msw';
-import { setupServer } from 'msw/node';
-import 'whatwg-fetch';
-
-afterEach(() => cache.clear());
-
-test('renders Form component', () => {
+test.skip('renders Form component', () => {
   render(<Form />);
 });
 
-// const server = setupServer(
-//   rest.get('/api/guests', (req, res, ctx) => {
-//     return res(
-//       ctx.status(200),
-//       ctx.json([
-//         {
-//           guest_party: ['THEO'],
-//           _id: '1111111111111',
-//           name: 'SERGIO DELGADO',
-//           rsvpd: false,
-//           phone: '',
-//         },
-//       ])
-//     );
-//   })
-// );
-
-// beforeAll(() => server.listen());
-// afterAll(() => server.close());
-// afterEach(() => server.resetHandlers());
-
-// describe('Database fetching', () => {
-//   beforeEach(() => {
-//     render(
-//       <SWRConfig value={{ dedupingInterval: 0 }}>
-//         <Form />
-//       </SWRConfig>
-//     );
-//   });
-
-test("Shows error message when database isn't loaded", async () => {
-  // server.use(
-  //   rest.get('api/guests', (req, res, ctx) => {
-  //     return res(ctx.status(500), ctx.json({ message: 'Error del servidor' }));
-  //   })
-  // );
+test.skip('Enables submit button when form is filled', () => {
   render(<Form />);
-  screen.debug();
-  fireEvent.change(screen.getByLabelText('Primer nombre:'), {
-    target: { value: 'Nayely' },
+
+  const button = screen.getByRole('button');
+
+  expect(button).toBeDisabled();
+
+  fireEvent.change(screen.getByLabelText(/Primer nombre:/), {
+    target: { value: 'SERGIO' },
   });
-  fireEvent.change(screen.getByLabelText('Apellido paterno:'), {
-    target: { value: 'Rodarte' },
+
+  fireEvent.change(screen.getByLabelText(/Apellido paterno:/), {
+    target: { value: 'DELGADO' },
   });
-  fireEvent.change(screen.getByLabelText('Teléfono celular (10 dígitos):'), {
-    target: { value: '1234567892' },
+
+  fireEvent.change(screen.getByLabelText(/Teléfono/gi), {
+    target: { value: '1234567891' },
   });
-  fireEvent.click(screen.getByRole('button'));
-  screen.debug();
+
+  expect(button).toBeEnabled();
 });
-// });
