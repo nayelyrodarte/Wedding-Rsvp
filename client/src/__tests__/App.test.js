3724330ce973f62422e9cbb2b80b881bbec9e7b9
Nayely Rodarte <nayelyrodarte@gmail.com>
2020-11-18 18:19:02 -0600

    Refactor App tests

--- client/src/__tests__/App.test.js
+++ client/src/__tests__/App.test.js
@@ -6,18 +6,66 @@ import {
   fireEvent,
   waitForElement,
   getByText,
+  waitForElementToBeRemoved,
 } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import { server, rest } from '../testServer';
+
+beforeAll(() => server.listen());
+afterAll(() => server.close());
+afterEach(() => server.resetHandlers());
 
 test('Succesfully renders App component', () => {
   render(<App />);
 });
 
-beforeEach(() => render(<App />));
+describe('Form handling', () => {
+  let submitButton;
+
+  beforeEach(() => {
+    render(<App />);
+
+    submitButton = screen.getByRole('button', { name: /buscar/i });
+
+    fireEvent.change(screen.getByLabelText(/Primer nombre:/), {
+    target: { value: 'SERGIO' },
+  });
+
+  fireEvent.change(screen.getByLabelText(/Apellido paterno:/), {
+    target: { value: 'DELGADO' },
+  });
+
+  fireEvent.change(screen.getByLabelText(/Teléfono/gi), {
+    target: { value: '1234567891' },
+  })}
+)
 
 test('Shows charging message when submit button is clicked', () => {
-  const button = screen.getByText('Buscar');
+  fireEvent.click(submitButton);
+
+  expect(screen.getByText(/Cargando.../)).toBeInTheDocument();
+});
+
+test("Shows checkboxes for guest's party when a guest is found", async () => {
+  fireEvent.click(submitButton);
+
+  await waitForElementToBeRemoved(() => screen.getByText(/Cargando.../));
+
+  server.use(
+    rest.get('api/guests', async (req, res, ctx) => {
+      return res(ctx.status(500));
+    })
+  );
+
+  waitForElement(() => expect(screen.getByText('THEO').toBeInTheDocument()));
+});
+
+})
+
+test.skip('Enables submit button when form is filled', () => {
+  const button = screen.getByRole('button', { name: /buscar/i });
+
+  expect(button).toBeDisabled();
 
   fireEvent.change(screen.getByLabelText(/Primer nombre:/), {
     target: { value: 'SERGIO' },
@@ -31,45 +79,5 @@ test('Shows charging message when submit button is clicked', () => {
     target: { value: '1234567891' },
   });
 
-  fireEvent.click(button);
-
-  expect(screen.getByText(/Cargando.../)).toBeInTheDocument();
-});
-
-beforeAll(() => server.listen());
-afterAll(() => server.close());
-afterEach(() => server.resetHandlers());
-
-describe('Database requests', () => {
-  test("Shows checkboxes for guest's party when a guest is found in database", async () => {
-    const button = screen.getByRole('button', { name: /buscar/i });
-
-    fireEvent.change(screen.getByLabelText(/Primer nombre:/), {
-      target: { value: 'SERGIO' },
-    });
-
-    fireEvent.change(screen.getByLabelText(/Apellido paterno:/), {
-      target: { value: 'DELGADO' },
-    });
-
-    fireEvent.change(screen.getByLabelText(/Teléfono/gi), {
-      target: { value: '1234567891' },
-    });
-
-    fireEvent.click(button);
-
-    // server.use(
-    //   rest.get('api/guests', async (req, res, ctx) => {
-    //     return res(ctx.status(500));
-    //   })
-    // );
-
-    screen.debug();
-
-    // await waitForElement(() =>
-    //   expect(screen.getByText('THEO').toBeInTheDocument())
-    // );
-
-    // screen.debug();
-  });
+  expect(button).toBeEnabled();
 });
